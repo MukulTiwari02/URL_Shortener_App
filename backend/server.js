c64562e88f1cf67be7ec7b70c6ed3f09c6cd9978
MukulTiwari02 <110053090+MukulTiwari02@users.noreply.github.com>
2023-08-16 00:36:20 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+require("dotenv").config();
+const connectDB = require("./config/connectDB")
+const urlRoutes = require("./routes/urlRoutes");
+const userRoutes = require("./routes/userRoutes");
+const { redirectToURL } = require("./controllers/urlControllers");
+const cookieParser = require("cookie-parser");
+
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false})) 
+app.use(cookieParser())
+
+
+app.use("/api/url", urlRoutes)
+app.use("/api/user", userRoutes)
+
+app.get("/" , (req,res)=> {
+    res.send("Home Page")
+})
+
+app.get("/:shortURL", redirectToURL)
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, ()=> {
+    connectDB(); 
+    console.log("Server running on PORT " + PORT);   
+})
